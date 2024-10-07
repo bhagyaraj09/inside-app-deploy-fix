a31d63a532b726ae58577b1b312f67707109719e
bhagyaraj09 <BhagyaRaj.Jujjavarapu@symphonize.com>
2024-10-07 14:45:04 +0530

    updated routes assigned leave page to all

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,4 +1,4 @@
-export const ExecutiveRoutes = ["/time", "/about", "/human-resources", "/holiday-schedule", "/"];
-export const EngagementManagerRoutes = ["/admin/approve-time", "/admin/time-report", '/time', "/about", "/human-resources", "/holiday-schedule", "/"];
-export const ProjectManagerRoutes = ["/time", "/about", "/human-resources", "/holiday-schedule", "/"];
-export const EmployeeRoutes = ["/time", "/about", "/human-resources", "/holiday-schedule", "/"];
+export const ExecutiveRoutes = ["/time", "/about","/leave", "/human-resources", "/holiday-schedule", "/"];
+export const EngagementManagerRoutes = ["/admin/approve-time", "/admin/time-report", '/time', "/about", "/leave","/human-resources", "/holiday-schedule", "/"];
+export const ProjectManagerRoutes = ["/time", "/about", "/human-resources", "/holiday-schedule", "/","/leave",];
+export const EmployeeRoutes = ["/time", "/about", "/human-resources", "/holiday-schedule", "/","/leave",];
